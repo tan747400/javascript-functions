7acc78c53195d47a78780aa53fe8ff5bce217f2a
Thanisorn <thanisorn.sir@gmail.com>
2025-07-18 14:35:14 +0700

    feat: update ex-1 to ex-6

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,25 @@
 // Start coding here
+const add = function (a, b) {
+    const result = a + b;
+    return result;
+  };
+  console.log(add(10, 5));
+  
+  const subtract = function (a, b) {
+    const result = a - b;
+    return result;
+  };
+  console.log(subtract(10, 5));
+  
+  const multiply = function (a, b) {
+    const result = a * b;
+    return result;
+  };
+  console.log(multiply(10, 5));
+  
+  const divide = function (a, b) {
+    const result = a / b;
+    return result;
+  };
+  console.log(divide(10, 5));
+  
